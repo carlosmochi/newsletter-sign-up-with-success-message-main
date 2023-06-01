f59b1380ad7e1fe86e2ae10dc952e938142e9c70
carlosmochi <carlosamochi@gmail.com>
2023-06-01 18:37:48 -0400

    challange finished

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 const func = {
     checkEmail: function(email){
+        event.preventDefault();
         if(email.value == "" || !email.validity.valid){
-            
             console.log("testando");
             email.classList.add("error")
             document.getElementsByClassName("email_warning")[0].removeAttribute("hidden")
@@ -10,8 +10,14 @@ const func = {
                 document.getElementsByClassName("email_warning")[0].setAttribute("hidden", true)
             }, 3000);
             event.preventDefault();
-        }
-        
+        }else{
+            document.getElementsByClassName("info_div")[0].setAttribute("hidden", true)
+            document.getElementsByClassName("image_div")[0].setAttribute("hidden", true)
+            document.getElementsByClassName("success_div")[0].removeAttribute("hidden")
+        }   
+    },
+    dismissMessage(){
+        location.reload();
     }
 } 
 window.func = func;
